7818ad498d952cb4c276fc560ca81d42500daf55
liyiming <liyiming0215@sina.com>
2018-01-22 10:12:20 +0800

    Revert "修改文件"
    This reverts commit 9c4a8ceb0c67f03def5098797e5348f09c8d4d5d.

--- src/jiuzhang/Solution.java
+++ src/jiuzhang/Solution.java
@@ -34,5 +34,6 @@ public class Solution {
     public static void main(String[] args) {
     	System.out.println(repeatedSubstringPattern("abab"));
     	String ss = "sssss";
+    	ss.indexOf(ch)("aaa");
     }
 }
