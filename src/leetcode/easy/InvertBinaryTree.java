c47075b317f79f7ec46ad1b8fb6c2b83070ca8f9
liyiming <liyiming0215@sina.com>
2018-05-23 15:38:16 +0800

    invert binary tree

---
+++ src/leetcode/easy/InvertBinaryTree.java
@@ -0,0 +1,61 @@
+/**
+* @author:liyiming
+* @date:2018年5月23日
+* Description:
+**/
+package leetcode.easy;
+
+/**
+ * Title: InvertBinaryTree Description:翻转一棵二叉树。 示例： 输入： 4 / \ 2 7 / \ / \ 1 3 6
+ * 9 输出： 4 / \ 7 2 / \ / \ 9 6 3 1 Company:pusense
+ * 
+ * @author ：lyiming
+ * @date ：2018年5月23日
+ **/
+public class InvertBinaryTree{
+
+	/**
+	 * @author:liyiming
+	 * @date:2018年5月23日
+	 * @Description:
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		TreeNode left = new TreeNode(2);
+		TreeNode right = new TreeNode(3);
+		root.left = left;
+		root.right = right;
+
+		TreeNode ll = new TreeNode(4);
+		TreeNode lr = new TreeNode(5);
+		TreeNode rl = new TreeNode(6);
+		TreeNode rr = new TreeNode(7);
+		left.left = ll;
+		left.right = lr;
+		right.left = rl;
+		right.right = rr;
+
+		System.out.println(root);
+		System.out.println(invertTree(root));
+	}
+
+	/**
+	 * 4251637
+	 * 
+	 * @author:liyiming
+	 * @date:2018年5月23日
+	 * @Description:
+	 * @param root
+	 * @return
+	 */
+	public static TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode temp = root.left;
+		root.left = invertTree(root.right);
+		root.right = invertTree(temp);
+		return root;
+	}
+}
--- src/leetcode/easy/TreeNode.java
+++ src/leetcode/easy/TreeNode.java
@@ -20,4 +20,42 @@ public class TreeNode{
 	TreeNode(int x) {
 		val = x;
 	}
+
+	/**
+	 * @author:liyiming
+	 * @date:2018年5月23日
+	 * @Description:
+	 * @return
+	 */
+	@Override
+	public String toString() {
+		int depth = getBinaryTreeDepth();
+		System.out.println(depth);
+		return toStringRecursion(this);
+	}
+	
+	private String toStringRecursion(TreeNode node) {
+		if(null == node) {
+			return "";
+		}
+		String ss = ""+toStringRecursion(node.left)+""+node.val+""+toStringRecursion(node.right);
+		return ss;
+	}
+	/**
+	 * 必须是一颗二叉树
+	 * 
+	 * @author:liyiming
+	 * @date:2018年5月23日
+	 * @Description:
+	 * @return
+	 */
+	private int getBinaryTreeDepth() {
+		TreeNode temp = this.left;
+		int depth = 1;
+		while (null != temp) {
+			depth++;
+			temp = temp.left;
+		}
+		return depth;
+	}
 }
