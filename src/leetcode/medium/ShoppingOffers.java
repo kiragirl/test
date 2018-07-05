503e20aa943bd8b16c97d64b3e4457224dac7f13
liyiming <liyiming0215@sina.com>
2018-07-05 09:22:42 +0800

    can place flowers

---
+++ src/leetcode/medium/ShoppingOffers.java
@@ -0,0 +1,29 @@
+/**
+* @author:liyiming
+* @date:2018年7月5日
+* Description:
+**/
+package leetcode.medium;
+
+
+/**
+* 	Title: ShoppingOffers
+*	Description:
+*	Company:pusense
+* 	@author ：lyiming
+* 	@date ：2018年7月5日
+**/
+public class ShoppingOffers{
+
+	/**
+	 * @author:liyiming
+	 * @date:2018年7月5日
+	 * @Description:
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
